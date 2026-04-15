a2cb6c1dc5d1fef412afd3046010a111d4be963d
fpondarts <perezflavio94@gmail.com>
2026-04-15 12:06:58 -0300

    Added tests worklow. Add broken code to check tests fail

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	headers := &http.Header{}
+
+	headers.Add("Authorization", "ApiKey 1234")
+
+	expected := "1234"
+
+	got, _ := GetAPIKey(*headers)
+
+	if expected != got {
+		t.Fatalf("Exppected %v, got %v", expected, got)
+	}
+}
+
+func TestGetAPIKeyNoHeader(t *testing.T) {
+	headers := &http.Header{}
+
+	got, err := GetAPIKey(*headers)
+
+	if got != "" && err == nil {
+		t.Fatalf("Expected error to be thrown")
+	}
+}
